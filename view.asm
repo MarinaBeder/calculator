202396020e659d9e82cad8b2e86a891f920f04e3
mona <Mona180874@feng.bu.edu.eg>
2021-12-15 22:46:15 +0200

    view
    view the result  on the screen

---
+++ view.asm
@@ -0,0 +1,14 @@
+View:  mov ax,dx
+       mov dx,0
+       div cx 
+       call ViewNo
+       mov bx,dx 
+       mov dx,0
+       mov ax,cx 
+       mov cx,10
+       div cx
+       mov dx,bx 
+       mov cx,ax
+       cmp ax,0
+       jne View
+       ret 
