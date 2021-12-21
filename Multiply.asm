0db2d9032c0b308c909df675a0bc6b178e958ac4
mona <Mona180874@feng.bu.edu.eg>
2021-12-22 01:07:28 +0200

    Multiply
    doing Multiply of two numbers

---
+++ Multiply.asm
@@ -0,0 +1,23 @@
+Multiply:   mov ah,09h
+            mov dx, offset msg2
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg3
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            mov ax,dx
+            mul bx 
+            mov dx,ax
+            push dx 
+            mov ah,9
+            mov dx, offset msg5
+            int 21h
+            mov cx,10000
+            pop dx
+            call View 
+            jmp exit 
