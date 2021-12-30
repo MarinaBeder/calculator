c3deb9b5211c69b11720e515e37af4d0c2f5b1ef
nihal <nehal180905@feng.bu.edu.eg>
2021-12-30 01:32:47 -0800

    subtract

---
+++ Subtract func.asm
@@ -0,0 +1,24 @@
+     Subtract:   mov ah,09h
+            mov dx, offset msg2
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg3
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            sub bx,dx
+            mov dx,bx
+            push dx 
+            mov ah,9
+            mov dx, offset msg5
+            int 21h
+            mov cx,10000
+            pop dx
+            call View 
+            jmp exit 
+
+
