c90a6f0456819a4d710531bea290eecbc172b0c2
mariam tarek <mariam180826@feng.bu.edu.eg>
2022-01-02 19:37:33 +0200

    FormNo

---
+++ FormNo.asm
@@ -0,0 +1,16 @@
+;we took each number separatly so we need to form our number and store in one bit for example if our number 235
+FormNo:     pop ax  
+            push dx      
+            mul bx
+            pop dx
+            add dx,ax
+            mov ax,bx       
+            mov bx,10
+            push dx
+            mul bx
+            pop dx
+            mov bx,ax
+            dec cx
+            cmp cx,0
+            jne FormNo
+            ret   
